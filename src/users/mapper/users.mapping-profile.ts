ab006f9afacb8e7b56bb517d5f0a77650adb68dd
Anton Veselev <wh4y@mail.ru>
2023-01-19 00:24:48 +0300

    UsersMappingProfile

--- src/users/mapper/users.mapping-profile.ts
+++ src/users/mapper/users.mapping-profile.ts
@@ -4,7 +4,7 @@ import { AutomapperProfile, InjectMapper } from '@automapper/nestjs';
 import { UserDto } from '../dtos/user.dto';
 import { User } from '../entities/user.entity';
 
-export class UsersMapperProfile extends AutomapperProfile {
+export class UsersMappingProfile extends AutomapperProfile {
   constructor(@InjectMapper() mapper: Mapper) {
     super(mapper);
   }
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,14 +3,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { UsersController } from './controllers/users.controller';
 import { User } from './entities/user.entity';
-import { UsersMapperProfile } from './mapper/users.mapper-profile';
+import { UsersMappingProfile } from './mapper/users.mapping-profile';
 import { UsersRepository } from './repositories/users.repository';
 import { UsersService } from './services/users.service';
 
 @Module({
   controllers: [UsersController],
   imports: [TypeOrmModule.forFeature([User])],
-  providers: [UsersService, UsersRepository, UsersMapperProfile],
-  exports: [UsersService, UsersMapperProfile],
+  providers: [UsersService, UsersRepository, UsersMappingProfile],
+  exports: [UsersService, UsersMappingProfile],
 })
 export class UsersModule {}
