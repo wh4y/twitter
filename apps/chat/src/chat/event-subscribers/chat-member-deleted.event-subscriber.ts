fd0c921cd1b64ccb9539ff4e1e3bff90e12afe33
Anton Veselev <wh4y@mail.ru>
2023-02-24 11:30:40 +0300

    quickfix

--- apps/chat/src/chat/event-subscribers/chat-member-deleted.event-subscriber.ts
+++ apps/chat/src/chat/event-subscribers/chat-member-deleted.event-subscriber.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
+import { OnEvent } from '@nestjs/event-emitter';
 
-import { ChatMemberDeletedEventPayload } from '../events/chat-member-deleted.event';
+import { CHAT_MEMBER_DELETED_EVENT, ChatMemberDeletedEventPayload } from '../events/chat-member-deleted.event';
 import { ChatClientService } from '../services/chat-client/chat-client.service';
 import { ChatRoomService } from '../services/chat-room/chat-room.service';
 
@@ -8,6 +9,7 @@ import { ChatRoomService } from '../services/chat-room/chat-room.service';
 export class ChatMemberDeletedEventSubscriber {
   constructor(private readonly chatClientService: ChatClientService, private readonly chatRoomService: ChatRoomService) {}
 
+  @OnEvent(CHAT_MEMBER_DELETED_EVENT)
   public async handleChatMemberDeleted({ member }: ChatMemberDeletedEventPayload): Promise<void> {
     const memberClients = await this.chatClientService.getClientsByUserId(member.userId);
 
